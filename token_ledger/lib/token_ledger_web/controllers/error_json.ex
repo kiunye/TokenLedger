1fd000bcb8285ab8acdc92a113640857ade67ac5
Kiunye Araya <kchrismucheke@gmail.com>
2026-09-01 12:30:55 +0300

    Remove duplicate controller files under controllers/api and controllers/error_*.ex

--- token_ledger/lib/token_ledger_web/controllers/error_json.ex
+++
@@ -1,9 +0,0 @@
-defmodule TokenLedgerWeb.ErrorJSON do
-  @moduledoc """
-  Renders JSON error payloads for the Phoenix endpoint.
-  """
-
-  def render(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
-  end
-end
